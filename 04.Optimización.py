3218a12e7aad65a751ed2bf802627397e4008e64
achiapp0 <150530291+achiapp0@users.noreply.github.com>
2025-12-12 17:07:16 -0300

    Update 04.Optimización.py

--- 04.Optimización.py
+++ 04.Optimización.py
@@ -1,1 +1,10 @@
-#jklkhjlkhlk
+# Verificar que el data frame contenga todas las variables y cálculos necesarios
+
+test_df = df[df['dataset'] == 'test']
+desired_dataset = pd.merge(test_df, all_predictions_df[['ds', 'yhat', 'campaign']], on=['ds', 'campaign'], how='left')
+desired_dataset['AdCost'] = desired_dataset['budget_USD'] / (desired_dataset['yhat'])
+
+print(desired_dataset.head())
+
+# ----------------------------------------------------------------------------------------------------- #
+
